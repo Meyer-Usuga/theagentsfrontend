5549c50ce044012d8e5434d1566f19c21663036c
Meyer-Usuga <usugarestrepomeyer@gmail.com>
2024-05-10 23:10:03 -0500

    Fixed a bug that did not update the navigation bar and validated that it redirects you to the application if you are logged in.

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -8,42 +8,46 @@ export class LoginService {
 
   constructor() { }
 
+  /** Método para generar un token de acceso 
+  * @author Meyer Usuga Restrepo <theagensfrontend>
+  */
+
+  createToken() {
+    let token = '';
+    for (let i = 0; i < 10; i++) {
+      let number = Math.floor(Math.random() * 50);
+      token = token + number;
+    }
+    return token;
+  }
+
+  /** Método para obtener el token de acceso 
+  * @author Meyer Usuga Restrepo <theagensfrontend>
+  */
+
+  getToken() {
+    let token = null;
+    token = localStorage.getItem('token');
+
+    return token;
+  }
+
   /** Método para hacer una petición al backend
- * para evaluar las credenciales del usuario
- * 
- */
+  * para evaluar las credenciales del usuario
+  *  @author Meyer Usuga Restrepo <theagentsfrontend>
+  */
 
   loginUsuer() {
     return 1;
   }
 
-  logoutUser(){
+  /** Método  para cerrar sesión de un empleado
+  * @author Meyer Usuga Restrepo <theagentsfrontend>
+  */
+
+  logoutUser() {
     localStorage.removeItem('serviceSelected');
     localStorage.removeItem('token');
-  }
-
-  /** Método para generar un token de acceso 
-   * @author Meyer Usuga Restrepo <theagensfrontend>
-  */
-
-  createToken(){
-    let token = '';
-    for(let i=0;i<10;i++){
-      let number = Math.floor(Math.random() * 50); 
-      token = token + number; 
-    } 
-    return token; 
-  }
-
-  /** Método para obtener el token de acceso 
-   * @author Meyer Usuga Restrepo <theagensfrontend>
-  */
- 
-  getToken(){
-    let token = null;
-    token = localStorage.getItem('token');
-    
-    return token;
   }
 
 }
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -37,6 +37,8 @@ export default class LoginComponent implements OnInit {
       username: ['', Validators.required],
       password: ['', Validators.required]
     })
+
+    this.validateLogin();
   }
 
   /** Método para loguear un usuario 
@@ -45,18 +47,31 @@ export default class LoginComponent implements OnInit {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      
+
       //lógica para suscribirse al servicio de login
-      
+
+      //si la respuesta del back fue positiva
       this.token = this.loginService.createToken();
       if (this.token) {
         localStorage.setItem('token', this.token);
       }
-      //solucionar bug, que cuando loguea el toolobar no se actualiza
-      this.router.navigate(['theagents/']);
+      //si todo esta bien, redirigimos 
+      const urlTree = this.router.createUrlTree(['theagents/']);
+      window.location.href = this.router.serializeUrl(urlTree);
     }
     else {
       window.alert('No se llenaron todos los campos');
+    }
+  }
+
+  /** Método que verifica si el usuario ya está logueado
+  * y lo redirige al schdule si ya lo está
+  * @author Meyer Usuga Restrepo <theagents>
+  */
+
+  validateLogin() {
+    if (this.loginService.getToken()) {
+      this.router.navigate(['theagents/schdule']);
     }
   }
 }
